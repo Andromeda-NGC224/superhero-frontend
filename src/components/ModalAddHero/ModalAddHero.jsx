b6e8416aa4a824f155d4bf019a1b84a59e58a599
Andromeda-NGC224 <shadim115115@gmail.com>
2024-10-30 22:47:25 +0200

    added modal glow border

--- src/components/ModalAddHero/ModalAddHero.jsx
+++ src/components/ModalAddHero/ModalAddHero.jsx
@@ -63,112 +63,121 @@ export const ModalAddHero = ({ onModalClose }) => {
 
   return (
     <div className={css.modalContainer}>
-      <div className={css.modalContainerWindow}>
-        <button className={css.modalCloseButton} onClick={onModalClose}>
-          ×
-        </button>
-        <p className={css.modalText}>Add a new Superhero</p>
-        <form className={css.modalForm} onSubmit={handleSubmit}>
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="nickname">
-              Nickname
-            </label>
-            <input
-              required
-              type="text"
-              id="nickname"
-              name="nickname"
-              className={css.modalFormInput}
-              value={superhero.nickname}
-              onChange={handleChange}
-            />
-            <MdDriveFileRenameOutline size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="real_name">
-              Real Name
-            </label>
-            <input
-              required
-              type="text"
-              id="real_name"
-              name="real_name"
-              className={css.modalFormInput}
-              value={superhero.real_name}
-              onChange={handleChange}
-            />
-            <SlNotebook size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="origin_description">
-              Origin Description
-            </label>
-            <textarea
-              required
-              id="origin_description"
-              name="origin_description"
-              className={css.modalFormTextarea}
-              value={superhero.origin_description}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="superpowers">
-              Superpowers
-            </label>
-            <input
-              required
-              type="text"
-              id="superpowers"
-              name="superpowers"
-              className={css.modalFormInput}
-              value={superhero.superpowers}
-              onChange={handleChange}
-            />
-            <GiPowerLightning size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="catch_phrase">
-              Catch Phrase
-            </label>
-            <input
-              required
-              type="text"
-              id="catch_phrase"
-              name="catch_phrase"
-              className={css.modalFormInput}
-              value={superhero.catch_phrase}
-              onChange={handleChange}
-            />
-            <MdRecordVoiceOver size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <div className={css.modalFormLabel}>Images</div>
-            <label className={css.modalFormLabelPhoto} htmlFor="images">
-              Select a photo to upload...
-              <input
-                type="file"
-                id="images"
-                name="images"
-                multiple
-                accept="image/*"
-                className={css.modalFormInputPhoto}
-                placeholder="Enter image URLs separated by commas"
-                onChange={handleFileChange}
-              />
-            </label>
-            <FaPhotoFilm size={18} className={css.modalFormIcon} />
-          </div>
-
-          <button type="submit" className={css.modalFormButton}>
-            Add Superhero
+      <div className={css.forGradient}>
+        <div className={css.gradientBackground}></div>
+        <div className={css.modalContainerWindow}>
+          <button className={css.modalCloseButton} onClick={onModalClose}>
+            ×
           </button>
-        </form>
+          <p className={css.modalText}>Add a new Superhero</p>
+          <form className={css.modalForm} onSubmit={handleSubmit}>
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="nickname">
+                Nickname
+              </label>
+              <input
+                required
+                type="text"
+                id="nickname"
+                name="nickname"
+                className={css.modalFormInput}
+                value={superhero.nickname}
+                onChange={handleChange}
+              />
+              <MdDriveFileRenameOutline
+                size={18}
+                className={css.modalFormIcon}
+              />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="real_name">
+                Real Name
+              </label>
+              <input
+                required
+                type="text"
+                id="real_name"
+                name="real_name"
+                className={css.modalFormInput}
+                value={superhero.real_name}
+                onChange={handleChange}
+              />
+              <SlNotebook size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label
+                className={css.modalFormLabel}
+                htmlFor="origin_description"
+              >
+                Origin Description
+              </label>
+              <textarea
+                required
+                id="origin_description"
+                name="origin_description"
+                className={css.modalFormTextarea}
+                value={superhero.origin_description}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="superpowers">
+                Superpowers
+              </label>
+              <input
+                required
+                type="text"
+                id="superpowers"
+                name="superpowers"
+                className={css.modalFormInput}
+                value={superhero.superpowers}
+                onChange={handleChange}
+              />
+              <GiPowerLightning size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="catch_phrase">
+                Catch Phrase
+              </label>
+              <input
+                required
+                type="text"
+                id="catch_phrase"
+                name="catch_phrase"
+                className={css.modalFormInput}
+                value={superhero.catch_phrase}
+                onChange={handleChange}
+              />
+              <MdRecordVoiceOver size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <div className={css.modalFormLabel}>Images</div>
+              <label className={css.modalFormLabelPhoto} htmlFor="images">
+                Select a photo to upload...
+                <input
+                  type="file"
+                  id="images"
+                  name="images"
+                  multiple
+                  accept="image/*"
+                  className={css.modalFormInputPhoto}
+                  placeholder="Enter image URLs separated by commas"
+                  onChange={handleFileChange}
+                />
+              </label>
+              <FaPhotoFilm size={18} className={css.modalFormIcon} />
+            </div>
+
+            <button type="submit" className={css.modalFormButton}>
+              Add Superhero
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/components/ModalUpdateHero/ModalUpdateHero.jsx
+++ src/components/ModalUpdateHero/ModalUpdateHero.jsx
@@ -78,107 +78,116 @@ export const ModalUpdateHero = ({ onModalClose, id }) => {
 
   return (
     <div className={css.modalContainer}>
-      <div className={css.modalContainerWindow}>
-        <button className={css.modalCloseButton} onClick={onModalClose}>
-          ×
-        </button>
-        <p className={css.modalText}>Update a Superhero</p>
-        <form className={css.modalForm} onSubmit={handleSubmit}>
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="nickname">
-              Nickname
-            </label>
-            <input
-              type="text"
-              id="nickname"
-              name="nickname"
-              className={css.modalFormInput}
-              value={superhero.nickname}
-              onChange={handleChange}
-            />
-            <MdDriveFileRenameOutline size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="real_name">
-              Real Name
-            </label>
-            <input
-              type="text"
-              id="real_name"
-              name="real_name"
-              className={css.modalFormInput}
-              value={superhero.real_name}
-              onChange={handleChange}
-            />
-            <SlNotebook size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="origin_description">
-              Origin Description
-            </label>
-            <textarea
-              id="origin_description"
-              name="origin_description"
-              className={css.modalFormTextarea}
-              value={superhero.origin_description}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="superpowers">
-              Superpowers
-            </label>
-            <input
-              type="text"
-              id="superpowers"
-              name="superpowers"
-              className={css.modalFormInput}
-              value={superhero.superpowers}
-              onChange={handleChange}
-            />
-            <GiPowerLightning size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <label className={css.modalFormLabel} htmlFor="catch_phrase">
-              Catch Phrase
-            </label>
-            <input
-              type="text"
-              id="catch_phrase"
-              name="catch_phrase"
-              className={css.modalFormInput}
-              value={superhero.catch_phrase}
-              onChange={handleChange}
-            />
-            <MdRecordVoiceOver size={18} className={css.modalFormIcon} />
-          </div>
-
-          <div className={css.modalFormContainer}>
-            <div className={css.modalFormLabel}>Images</div>
-            <label className={css.modalFormLabelPhoto} htmlFor="images">
-              Select a photo to upload...
-              <input
-                type="file"
-                id="images"
-                name="images"
-                multiple
-                accept="image/*"
-                className={css.modalFormInputPhoto}
-                placeholder="Enter image URLs separated by commas"
-                onChange={handleFileChange}
-              />
-            </label>
-            <FaPhotoFilm size={18} className={css.modalFormIcon} />
-          </div>
-
-          <button type="submit" className={css.modalFormButton}>
-            Update Superhero
+      <div className={css.forGradient}>
+        <div className={css.gradientBackground}></div>
+        <div className={css.modalContainerWindow}>
+          <button className={css.modalCloseButton} onClick={onModalClose}>
+            ×
           </button>
-        </form>
+          <p className={css.modalText}>Update a Superhero</p>
+          <form className={css.modalForm} onSubmit={handleSubmit}>
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="nickname">
+                Nickname
+              </label>
+              <input
+                type="text"
+                id="nickname"
+                name="nickname"
+                className={css.modalFormInput}
+                value={superhero.nickname}
+                onChange={handleChange}
+              />
+              <MdDriveFileRenameOutline
+                size={18}
+                className={css.modalFormIcon}
+              />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="real_name">
+                Real Name
+              </label>
+              <input
+                type="text"
+                id="real_name"
+                name="real_name"
+                className={css.modalFormInput}
+                value={superhero.real_name}
+                onChange={handleChange}
+              />
+              <SlNotebook size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label
+                className={css.modalFormLabel}
+                htmlFor="origin_description"
+              >
+                Origin Description
+              </label>
+              <textarea
+                id="origin_description"
+                name="origin_description"
+                className={css.modalFormTextarea}
+                value={superhero.origin_description}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="superpowers">
+                Superpowers
+              </label>
+              <input
+                type="text"
+                id="superpowers"
+                name="superpowers"
+                className={css.modalFormInput}
+                value={superhero.superpowers}
+                onChange={handleChange}
+              />
+              <GiPowerLightning size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <label className={css.modalFormLabel} htmlFor="catch_phrase">
+                Catch Phrase
+              </label>
+              <input
+                type="text"
+                id="catch_phrase"
+                name="catch_phrase"
+                className={css.modalFormInput}
+                value={superhero.catch_phrase}
+                onChange={handleChange}
+              />
+              <MdRecordVoiceOver size={18} className={css.modalFormIcon} />
+            </div>
+
+            <div className={css.modalFormContainer}>
+              <div className={css.modalFormLabel}>Images</div>
+              <label className={css.modalFormLabelPhoto} htmlFor="images">
+                Select a photo to upload...
+                <input
+                  type="file"
+                  id="images"
+                  name="images"
+                  multiple
+                  accept="image/*"
+                  className={css.modalFormInputPhoto}
+                  placeholder="Enter image URLs separated by commas"
+                  onChange={handleFileChange}
+                />
+              </label>
+              <FaPhotoFilm size={18} className={css.modalFormIcon} />
+            </div>
+
+            <button type="submit" className={css.modalFormButton}>
+              Update Superhero
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
